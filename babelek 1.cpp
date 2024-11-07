f7ec7d5c867e659791f63e52b062cca2f9a15be7
Danrejk <WIKJERY44@edu.gdansk.pl>
2024-11-07 08:28:36 +0100

    init

--- babelek 1.cpp
+++ babelek 1.cpp
@@ -1,12 +1,13 @@
 ﻿#include <iostream>
 
-void bubble_sort_for_for_index(int n, int* Tab) {
+template <typename T>
+void bubble_sort_for_for_index(size_t n, T *tab) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1; j++) {
-            if (Tab[j] > Tab[j+1]) {
-                int tmp = Tab[j + 1];
-                Tab[j + 1] = Tab[j];
-                Tab[j] = tmp;
+            if (tab[j] > tab[j+1]) {
+                int tmp = tab[j + 1];
+                tab[j + 1] = tab[j];
+                tab[j] = tmp;
             }
         }
     }
@@ -45,10 +46,13 @@ void bubble_sort_while_shorter_for_index(int n, int* Tab) {
 }
 int main()
 {
-    int list[10] = { 10, 9, 8, 6, 7,8, 9, 3,2, 1 };
+    int list[10] = { 10, 9, 8, 6, 7,8, 9, 3, 2, 1 };
+
     bubble_sort_while_for_index(10, list);
     for (int i = 0; i < 10; i++) {
         std::cout << list[i] << std::endl;
     }
     
+
+    return 0;
 }
